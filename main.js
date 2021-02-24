3af24fdd351f1c43164a9d4b77a6109ee5fa3970
Slawek Wojtaszek <wojtaszek@protonmail.com>
2021-02-24 21:13:20 +0000

    Optimize Code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const start = document.querySelector("button.start");
+const btnStart = document.querySelector("button.start");
 const reset = document.querySelector(".reset");
 const div = document.querySelector(".time div");
 const lap = document.querySelector(".lap");
@@ -6,38 +6,23 @@ const lapsUl = document.querySelector(".laps");
 const stop = document.querySelector(".stop");
 const lapResults = [];
 
-let activeCounter;
+let activeCounter = false;
 let startTime = 0;
 let idI;
 let lapNumber = 0;
-let lapFlag = true;
-let mainButton = (start.textContent = "Start");
+let flag = false;
+let mainButton = (btnStart.textContent = "Start");
 
 div.textContent = "00:00.00";
 
-const startStoper = () => {
-   if (activeCounter === undefined) {
-      activeCounter = true;
-      start.textContent = "Stop";
-      start.style.backgroundColor = "rgb(106, 37, 37)";
-      start.style.color = "red";
-   } else if (start.textContent === "Stop" && activeCounter === true) {
-      activeCounter = undefined;
-      start.textContent = "Start";
-      // reset.textContent = "Reset";
-      start.style.backgroundColor = "rgb(34, 75, 34)";
-      start.style.color = "greenyellow";
-      // reset.style.zIndex = "2";
-   }
-};
+// Clear all laps
+function deleteLaps() {
+   lapsUl.remove();
+   startTime = 0;
+   div.textContent = "00:00.00";
+}
 
-const changeBeh = () => {
-   if (startTime > 0 && activeCounter === undefined) {
-      lap.textContent = "Reset";
-   }
-};
-
-// console.log(activeCounter);
+// Update time
 const updateStopper = () => {
    if (activeCounter === true) {
       startTime++;
@@ -45,57 +30,77 @@ const updateStopper = () => {
    }
 };
 
-function deleteLaps() {
-   lapsUl.remove();
-   startTime = 0;
-   div.textContent = "00:00.00";
-   // console.log("nie usuwam");
-}
-
-const addLap = () => {
-   if (startTime > 0 && activeCounter === true) {
-      const lapScore = (startTime / 100).toFixed(3);
-      lapResults.push(lapScore);
-      lapNumber++;
-      let newLi = document.createElement("li");
-      newLi.textContent = `Lap ${lapNumber}: ${lapScore}`;
-      lapsUl.appendChild(newLi);
-   }
-
-   if (
-      (lapResults.length > 0 && lap.textContent === "Reset") ||
-      (lapResults.length < 0 && lap.textContent === "Reset")
-   ) {
-      // activeCounter = undefined;
-      deleteLaps();
-
-      div.textContent = "00:"(startTime / 100).toFixed(3);
-      startTime = 0;
-   } else if (lapResults.length > 0 && startTime > 0) {
-      lap.textContent = "ero";
-      startTime = 0;
-   }
-};
-
-const btnReset = () => {
-   activeCounter = undefined;
-   startTime = 0;
-   div.textContent = "00:00.00";
-   start.textContent = "Start";
-   // lapResults = [];
-   clearInterval(idI);
-};
-
+// Run timer
 setInterval(updateStopper, 10);
 
-// const stopTimer = () => {
-//    console.log("stop");
-// };
+// Create new lap and add to an array and DOM
+const createNewLap = () => {
+   const lapScore = (startTime / 100).toFixed(3);
+   lapResults.push(lapScore);
+   lapNumber++;
+   let newLi = document.createElement("li");
+   newLi.textContent = `Lap ${lapNumber}: ${lapScore}`;
+   lapsUl.appendChild(newLi);
+};
 
-start.addEventListener("click", startStoper);
-start.addEventListener("click", changeBeh);
-// reset.addEventListener("click", btnReset);
-// reset.addEventListener("click", deleteLaps);
+//Clear laps
+
+const clearLaps = () => {
+   if (lap.textContent === "Lap") {
+      console.log("lap");
+   } else if (lap.textContent === "Reset") {
+      deleteLaps();
+      btnStart.textContent = "Start";
+   }
+};
+
+// Reset / Lap
+const resetLap = () => {
+   if (activeCounter === true) {
+      btnStart.textContent = "Start";
+      btnStart.style.backgroundColor = "rgb(34, 75, 34)";
+      btnStart.style.color = "greenyellow";
+      activeCounter = false;
+   }
+
+   console.log("elo");
+};
+
+// Handle Change
+
+const handleChange = () => {
+   if (activeCounter === true) {
+      btnStart.textContent = "Stop";
+      btnStart.style.backgroundColor = "rgb(106, 37, 37)";
+      btnStart.style.color = "red";
+   } else if (activeCounter === false) {
+      btnStart.textContent = "Start";
+      btnStart.style.backgroundColor = "rgb(106, 37, 37)";
+      btnStart.style.color = "greem";
+   }
+};
+
+//Handle start button
+const startStoper = () => {
+   if (activeCounter === false) {
+      activeCounter = true;
+      handleChange();
+   } else if (activeCounter === true) {
+      // lap.textContent === "LAP";
+      resetLap();
+   }
+};
+
+//Handle lap button
+const addLap = () => {
+   if (activeCounter === true) {
+      createNewLap();
+   }
+
+   // clearLaps();
+};
+
+// Event Listeners
+
+btnStart.addEventListener("click", startStoper);
 lap.addEventListener("click", addLap);
-// lap.addEventListener("click", btnReset);
-// stop.addEventListener("click", stopTimer);
